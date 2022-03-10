3f1f5d19dbe63fb0fa33026f938ccfe9e59d62f5
Jimmy Lingan <jimmy.lingan@outlook.com>
2022-03-10 12:07:55 -0500

    add pytest

---
+++ config/dashboard/tests/test_selenium_dashboard.py
@@ -0,0 +1,6 @@
+import pytest
+from selenium.webdriver.common.keys import Keys
+
+@pytest.mark.selenium
+def test_dashboard_admin_login(chrome_browser_instance):
+    pass
---
+++ conftest.py
@@ -0,0 +1,3 @@
+pytest_plugins = [
+    "config.tests.selenium",
+]
